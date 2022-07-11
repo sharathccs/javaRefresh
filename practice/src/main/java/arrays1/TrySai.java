ca056ca3b1201a216178bcff840c86082e97f2e4
schikkajalash <schikkajalash@10.109.245.195>
2022-07-11 18:57:49 +0530

    New class file committed.

---
+++ practice/src/main/java/arrays1/TrySai.java
@@ -0,0 +1,19 @@
+package arrays1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TrySai {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sai");
+		
+		
+
+	}
+	
+		
+
+}
