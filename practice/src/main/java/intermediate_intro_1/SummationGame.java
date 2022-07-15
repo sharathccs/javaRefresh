e242352cf347105ba6ba6549152d6e967a0e5c2f
schikkajalash <schikkajalash@10.109.245.195>
2022-07-15 06:03:01 +0530

    SummationGame: Sum of N natural numbers

---
+++ practice/src/main/java/intermediate_intro_1/SummationGame.java
@@ -0,0 +1,38 @@
+
+/*
+Write a program to print all odd numbers from 1 to N where you have to take N as input from user.
+
+*/
+
+package intermediate_intro_1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.math.*;
+
+
+public class SummationGame {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		try {
+				int N = sc.nextInt();
+				if(N<=10000 && N>=1) {
+					System.out.println(sumOfNaturalNumbers(N));
+				}
+			else 
+				System.out.println("Enter number within range");
+			
+		}catch(InputMismatchException e) {
+			System.out.println("Enter an integer");
+		}	
+
+	}
+	
+	public static int sumOfNaturalNumbers(int N) {
+		long Sum=0;
+		Sum=(N*(N+1)/2);
+		return (int)Sum;
+	}
+}
