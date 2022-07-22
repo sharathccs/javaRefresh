b41f82393198c4d4e542aa0d0a4b5d2a71ad7a4c
Sharath Chandra C S <sharathchandracs@gmail.com>
2022-07-22 18:13:01 +0530

    Rotation game

--- practice/src/main/java/intermediate_arrays/RotationGame.java
+++ practice/src/main/java/intermediate_arrays/RotationGame.java
@@ -1,3 +1,10 @@
+/*
+
+Given an integer array A of size N and an integer B, you have to print the same array after rotating it B times towards the right
+
+*/
+
+
 package intermediate_arrays;
 
 import java.util.InputMismatchException;
@@ -6,7 +13,7 @@ import java.util.Scanner;
 public class RotationGame {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		///Scan the below inputs
 		
@@ -15,39 +22,48 @@ public class RotationGame {
 		int sizeOfArray;
 		
 		//array
-		int [] arr = new int[sizeOfArray];
+		int [] arr ;
 	//	int [] Rotated_arr = new int[sizeOfArray];
 		
 		//	Number of times of rotation
 		int NumberOfRotations ;
+		int EffectiveRotations ;
 		
 
 		
 		Scanner sc = new Scanner(System.in);
 		try {
 				
-				System.out.println("Enter the size of array you want to construct:");
+				//System.out.println("Enter the size of array you want to construct:");
 				sizeOfArray	 = sc.nextInt();
 				
 				
-				System.out.println("Enter the array of size:"+sizeOfArray);
+				//System.out.println("Enter the array of size:"+sizeOfArray);
+				arr = new int[sizeOfArray];
+				
 				for(int i=0; i<arr.length; i++) {
 					 arr[i]=sc.nextInt();
 				 }
 				
-				System.out.println("Enter the number of rotations:");
+				//System.out.println("Enter the number of rotations:");
 				 NumberOfRotations = sc.nextInt();
-				
+				 EffectiveRotations = NumberOfRotations%sizeOfArray;
+				 int m=0;
+				 int [] Rotated_arr=new int[sizeOfArray];
 				 
 				 
-				
-				 int [] Rotated_arr=RotateArray(arr, NumberOfRotations);
-				
-				 System.out.println("Rotated Array:");
-				 for(int k: Rotated_arr) {
-					 System.out.println(k);
+				 
+				 if(EffectiveRotations==0) {
+					 printArray(arr);
+				 }else {
+					 while(m<EffectiveRotations){
+								Rotated_arr=RotateArray(arr);
+								 m++;
+								 arr=Rotated_arr;	 
+					 }
+					 printArray(Rotated_arr);
+					
 				 }
-				 
 				
 			}catch(InputMismatchException e) {
 			System.out.println("Enter an integer");
@@ -59,20 +75,26 @@ public class RotationGame {
 	}
 	
 
-			private static int[] RotateArray(int [] InputArr, int rotations) {
-			
-				int l = InputArr.length;
-				//int effectiveRotations=rotations%l;
-				int [] RotateArr = new int[l];
+			private static int[] RotateArray(int [] InputArr) {
+					int l = InputArr.length;
+					int [] RotateArr = new int[l];
+		
+					RotateArr[0]=InputArr[l-1];
+					for(int k=1, s=0; (k<l) || (s<l-1); k++, s++) {
+						RotateArr[k]=InputArr[s];
+					}					
+					return RotateArr;
 				
-				
-				RotateArr[0]=InputArr[l-1];
-				for(int k=1, s=0; (k<l) || (s<l-1); k++, s++) {
-					RotateArr[k]=InputArr[s];
-				}
+			}
 			
-								
-				return RotateArr;
-			}	
+			static void printArray(int [] ArrayToBePrinted) {
+				
+				for(int k: ArrayToBePrinted) {
+					 System.out.println(k);
+				 }
+				
+			}
+			
+			
 
 }
